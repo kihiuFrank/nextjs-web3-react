f3f1763a469ac2a4e47eac08022cb330acd138c1
kihiuFrank <franklinekihiu@gmail.com>
2022-09-27 11:19:26 +0300

    web3-react in nextjs / react

---
+++ pages/_app.js
@@ -0,0 +1,33 @@
+import "../styles/globals.css";
+import { Web3ReactProvider } from "@web3-react/core";
+import { Web3Provider } from "@ethersproject/providers";
+
+const getLibrary = (provider) => {
+  return new Web3Provider(provider);
+};
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Component {...pageProps} />
+    </Web3ReactProvider>
+  );
+}
+
+export default MyApp;
+
+/* import "../styles/globals.css";
+import { Web3ReactProvider } from "@web3-react/core";
+import { Web3Provider } from "@ethersproject/providers";
+
+const getLibrary = (provider) => {
+  return new Web3Provider(provider);
+};
+
+function MyApp({ Component, pageProps }) {
+  <Web3ReactProvider getLibrary={getLibrary}>
+    return <Component {...pageProps} />;
+  </Web3ReactProvider>;
+}
+
+export default MyApp; */
